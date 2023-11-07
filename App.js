6bd5b397eb71a0c871dde6e3469d96fff38b75de
darylng154 <daryl.ng154@gmail.com>
2023-11-06 17:50:35 -0800

    testing image component

--- App.js
+++ App.js
@@ -1,4 +1,60 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, ImageBackground} from 'react-native';
+
+// local disk
+// Note: camera roll is different
+const logoLink = require("./assets/adaptive-icon.png");
+
+const logoImg = 
+  <Image
+    source = {logoLink}
+    style = 
+    {
+      {
+        width: 300,
+        height: 300
+      }
+    }
+  />;
+
+// network
+const netLink = "https://i.ytimg.com/vi/xXmXM0qRMbo/maxresdefault.jpg";
+
+const netImg = 
+  <Image
+  // need to convert image string into object for source
+    source = {{uri: netLink}}
+    style = 
+    {
+      {
+        width: 300,
+        height: 300,
+        paddingTop: 100
+      }
+    }
+  />;
+
+// image background w/ text
+const imgBckgnd = 
+    <ImageBackground
+      source = {logoLink}
+      // make image take up avaiable space
+      style = 
+      {
+        {
+          flex: 1
+        }
+      }>
+        <Text
+          style = 
+          {
+            {
+              color: "white",
+              textAlign: "center"
+            }
+          }>
+        Image Text
+        </Text>
+    </ImageBackground>
 
 export default function App()
 {
@@ -13,15 +69,12 @@ export default function App()
           paddingVertical: 100
         }
       }>
-      <Text
-        style = 
-        {
-          {
-            color: "white"
-          }
-        }>
-        Hello World
-      </Text>
+      
+      {/* can store components inside variable*/}
+      {/*logoImg*/}
+      {/*netImg*/}
+      {imgBckgnd}
+  
     </View>
   );
 };
